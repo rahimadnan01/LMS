ef860e8f969f1a6b1c02fd8415e5f5188761d307
Rahim Adnan <hafizrahim3011@gmail.com>
2025-01-29 06:57:25 +0500

    Add admin Panel

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Header() {
-  return <div className="w-full h-screen bg-pink-400"></div>;
+  return (
+    <div className="w-full h-screen  flex justify-center items-center">
+      <p>Hello i am header</p>
+    </div>
+  );
 }
 
 export default Header;
